4144915d2ada5b678e16d019ba0fe0a40d45b707
Jeremy Bilger <bilgerjeremy5705@gmail.com>
2025-06-27 11:06:29 +0200

    Update and place the page at the center

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen flex flex-col items-center">
       <Navbar />
       <main>
         <Hero />
